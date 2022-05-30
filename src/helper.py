3c21d5e944ba1b3bd7e7633969a40410a1c767c4
Josef <jdhamelink0@gmail.com>
2022-05-31 01:09:53 +0200

    Add timer
    - test function to showcase usage
    - slightly improve readability
    - update gitignore

--- src/helper.py
+++ src/helper.py
@@ -4,8 +4,10 @@ Helper
 Data Science Assignment 3 - Home Depot Search Results
 """
 
-from io import TextIOWrapper	# type hinting
-import os, sys, re       		# directories
+import os, sys, re				# directories
+from datetime import datetime	# printing experiment starting time
+import time						# getting time indications during the experiment
+import pandas as pd				# reading in data
 
 BOLD = lambda string: f'\033[1m{string}\033[0m'
 
@@ -13,25 +15,51 @@ def fix_dirs() -> None:
 	"""Changes cwd to src, and creates a results dir on the same level if not already present"""
 	cwd = os.getcwd()
 	if cwd.split(os.sep)[-1] != 'src':
+		
 		if not os.path.exists(os.path.join(cwd, 'src')):
 			print(f'Please work from either the parent directory "{BOLD("Home-Depot")}",',
 				  f'or from "{BOLD("src")}" in order to run any scripts that are in "src".')
 			sys.exit(1)
+		
 		os.chdir(os.path.join(cwd, 'src'))
 		cwd = os.getcwd()
 		caller = re.search(r'src(.*?).py', str(sys._getframe(1))).group(1)[1:] + '.py'
 		print(f'\n WARNING: Working directory changed to "{cwd}".',
 			  f'Consider running {BOLD(caller)} directly from "src" dir next time.\n')
+	
 	if not os.path.exists(results_dir := os.path.join(cwd, '..', 'results')):
 		os.mkdir(results_dir)
 
-def load_data(filenames: list[str]) -> list[TextIOWrapper]:
-	"""Loads given csv files into a list as TextIOWrappers"""
-	data_dir: str = os.path.join(os.getcwd(), '..', 'data')
-	files: list[TextIOWrapper] = []
+def load_data(filenames: list[str], sample: bool = False) -> list[pd.DataFrame]:
+	"""Loads given csv files into a list as pandas DataFrames, don't put .csv in the filename"""
+	if sample:
+		data_dir: str = os.path.join(os.getcwd(), '..', 'sample_data')
+	else:
+		data_dir: str = os.path.join(os.getcwd(), '..', 'data')
+	
+	files: list[pd.DataFrame] = []
 	for filename in filenames:
-		try: files.append(open(os.path.join(data_dir, filename), 'r', encoding='ISO-8859-1'))
+		try:
+			filepath = os.path.join(data_dir, filename+'.csv')
+			files.append(pd.read_csv(filepath, encoding='ISO-8859-1'))
 		except FileNotFoundError:
 			print(f'No file called {BOLD(filename)} is present in the data directory.')
 			sys.exit(1)
 	return files
+
+class Timer:
+	def __init__(self, first_process: str) -> None:
+		"""Sets up a timer object and prints the name of the first process"""
+		print(f'\nexperiment started at {datetime.now().strftime("%H:%M:%S")}')
+		print(f'\n{first_process}: ', end='')
+		self.tic: float = time.perf_counter()
+
+	def __call__(self, next_process: str = None) -> None:
+		"""Prints the time it took to complete the previous process and if specified, the name of the next process"""
+		passed: float = round((toc := time.perf_counter()) - self.tic, 6)
+		print(f'{BOLD(passed)} s', end='')
+		if next_process is not None:
+			print(f'\n{next_process}: ', end='')
+			self.tic = toc
+		else:
+			print()
--- src/main.py
+++ src/main.py
@@ -7,13 +7,33 @@ Data Science Assignment 3 - Home Depot Search Results
 import numpy as np
 import pandas as pd
 import spacy
-from helper import fix_dirs, load_data
+from helper import fix_dirs, load_data, Timer
 
 def main():
 	fix_dirs()
-	data = load_data(['test.csv'])
-	test = data[0]
-	print(test.readlines()[0])
+	test()
+
+def test():
+
+	timer = Timer(first_process='reading in train, test & descriptions')
+
+	train, test, descriptions = load_data(['train', 'test', 'product_descriptions'])
+	
+	timer(next_process='reading in attributes')
+	
+	attributes = load_data(['attributes'])[0]
+
+	timer()
+
+	print('\ntest')
+	print(test.head(1))
+	print('\ntrain')
+	print(train.head(1))
+	print('\ndescriptions')
+	print(descriptions.head(1))
+	print('\nattributes')
+	print(attributes.head(1))
+
 
 if __name__ == "__main__":
 	main()
