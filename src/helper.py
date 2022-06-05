ad40384088e1763ed12271e216c805e73342f07b
Josef <jdhamelink0@gmail.com>
2022-06-05 22:22:16 +0200

    QoL changes
    - added lru_cache
    - fixed full & sample docbins writing to same directory
    - added instructions for running main

--- src/helper.py
+++ src/helper.py
@@ -11,13 +11,14 @@ import argparse
 import os, sys, re, shutil		# directories
 from datetime import datetime	# printing experiment starting time
 import time						# getting time indications during the experiment
+from functools import lru_cache	# speeding up nlp parsing
 # dependencies
 import pandas as pd				# reading in data
 import spacy
 
 BOLD = lambda string: f'\033[1m{string}\033[0m'
 
-def parse_wrapper(parser: argparse.ArgumentParser) -> bool:
+def argparse_wrapper(parser: argparse.ArgumentParser) -> bool:
 	"""Returns the parsed arguments of the file"""
 	parser.add_argument('-f', '--full', action='store_true',
 						help=('run script on full dataset, default is to run on sample data'))
@@ -87,7 +88,8 @@ def load_dataframes(filenames: list[str], full: bool = False) -> dict[str, pd.Da
 	
 	return dataframes
 
-def parse_dataframes(dataframes: dict[str, pd.DataFrame], colnames: list[str], nlp: spacy.Language) -> dict[str, pd.DataFrame]:
+def parse_dataframes(dataframes: dict[str, pd.DataFrame], colnames: list[str],
+					 nlp: spacy.Language) -> dict[str, pd.DataFrame]:
 	"""
 	In the given dataframes, converts the string values in the given dataframes into spaCy `doc` objects.
 	---
@@ -101,16 +103,22 @@ def parse_dataframes(dataframes: dict[str, pd.DataFrame], colnames: list[str], n
 	### returns
 		- dataframes: the same dict, but with parsed dataframes
 	"""
+
+	@lru_cache(maxsize=50)
+	def _nlp_wrapper(string: str) -> spacy.tokens.Doc:
+		"""small wrapper so caching can provide ±30% speed improvement"""
+		return nlp(string)
+
 	for df_name, df in dataframes.items():
 		for col in colnames:
-			try: df[col] = df[col].apply(nlp)
+			try: df[col] = df[col].apply(_nlp_wrapper)
 			except KeyError:
 				print(f'Dataframe "{BOLD(df_name)}" has no column called "{BOLD(col)}"')
 				sys.exit(1)
 	
 	return dataframes
 
-def store_docs_as_docbins(dataframes: dict[str, pd.DataFrame], colnames: list[str]) -> None:
+def store_docs_as_docbins(dataframes: dict[str, pd.DataFrame], colnames: list[str], full: bool = False) -> None:
 	"""
 	Stores the spaCy `Doc` objects present in the given dataframes on the user's disk as `DocBin` objects
 	---
@@ -119,20 +127,23 @@ def store_docs_as_docbins(dataframes: dict[str, pd.DataFrame], colnames: list[st
 			* keys: the names of the dataframes containing `Doc` objects to be stored
 			* values: the actual dataframes
 		- colnames: list of column names containing `Doc` objects
+		- full: set to True to run on the entire dataset
 	"""
-
-	if os.path.exists((db_dir := os.path.join('..','docbins'))):
+	flag = '_sample' if not full else ''
+	if os.path.exists((db_dir := os.path.join('..','docbins'+flag))):
 		shutil.rmtree(db_dir)
 	os.mkdir(db_dir)
 
-	doc_bins = {df_name: {col: spacy.tokens.DocBin(docs=df[col]) for col in colnames} for df_name, df in dataframes.items()}
+	doc_bins = {df_name: {col: spacy.tokens.DocBin(docs=df[col]) \
+				for col in colnames} for df_name, df in dataframes.items()}
 	for df_name, col_dict in doc_bins.items():
 		if not os.path.exists((df_dir := os.path.join(db_dir,df_name))):
 			os.mkdir(df_dir)
 		for col, doc_bin in col_dict.items():
 			doc_bin.to_disk(os.path.join(df_dir,col+'.spacy'))
 
-def create_doc_dataframes(dataframes: dict[str, pd.DataFrame], colnames: list[str], nlp: spacy.Language) -> dict[str, pd.DataFrame]:
+def create_doc_dataframes(dataframes: dict[str, pd.DataFrame], colnames: list[str],
+						  nlp: spacy.Language, full: bool = False) -> dict[str, pd.DataFrame]:
 	"""
 	Replaces the string data in the given dataframes with corresponding spaCy `Doc` objects present on the user's disk
 	---
@@ -142,11 +153,13 @@ def create_doc_dataframes(dataframes: dict[str, pd.DataFrame], colnames: list[st
 			* values: the actual dataframes
 		- colnames: list of column names of which the string data should be replaced with corresponding `doc` objects
 		- nlp: the spaCy language object used to parse the strings
+		- full: set to True to run on the entire dataset
 	
 	### returns
 		- dataframes: the same dict, but with parsed dataframes
 	"""
-	db_dir = os.path.join(os.getcwd(),'..','docbins')
+	flag = '_sample' if not full else ''
+	db_dir = os.path.join(os.getcwd(),'..','docbins'+flag)
 	doc_bins = {df_name: {col: spacy.tokens.DocBin().from_disk(os.path.join(db_dir,df_name,col+'.spacy')) \
 				for col in colnames} for df_name in dataframes.keys()}
 
--- src/main.py
+++ src/main.py
@@ -12,11 +12,14 @@ import argparse
 import pandas as pd
 import spacy
 # local imports
-from helper import parse_wrapper, fix_dirs, Timer
+from helper import argparse_wrapper, fix_dirs, Timer
 from helper import load_dataframes, parse_dataframes, store_docs_as_docbins, create_doc_dataframes
 
 def main():
-	full: bool = parse_wrapper(argparse.ArgumentParser())
+
+	print(f'pandas: v{pd.__version__}, spaCy: v{spacy.__version__}')
+
+	full: bool = argparse_wrapper(argparse.ArgumentParser())
 	fix_dirs()
 	colnames = ['product_title', 'search_term']
 	nlp: spacy.Language = spacy.load('en_core_web_lg')
@@ -24,19 +27,21 @@ def main():
 	timer = Timer(first_process='reading csv files')
 	dataframes: dict[str, pd.DataFrame] = load_dataframes(['train', 'test'], full=full)
 
+	# code block between the separators is responsible for parsing relevant strings and storing this
+	# when the data has been stored successfully, it can be safely commented out and main can be rerun
 	# ------------------------------------------------------------------------------------------------
 	timer('parsing')
 	dataframes: dict[str, pd.DataFrame] = parse_dataframes(dataframes, colnames, nlp)
 
 	timer('storing as docs')
-	store_docs_as_docbins(dataframes, colnames)
+	store_docs_as_docbins(dataframes, colnames, full=full)
 
-	# timer()
-	# quit()
+	timer()
+	quit()
 	# ------------------------------------------------------------------------------------------------
 
 	timer('reading docs back in')
-	dataframes: dict[str, pd.DataFrame] = create_doc_dataframes(dataframes, colnames, nlp)
+	dataframes: dict[str, pd.DataFrame] = create_doc_dataframes(dataframes, colnames, nlp, full=full)
 
 	timer()
 
@@ -44,9 +49,13 @@ def main():
 		print()
 		print(df_name)
 		for _, row in dataframe.head(10).iterrows():
-			# little bit of testing
+			# check if similarity functions work
 			st, pt = row['search_term'], row['product_title']
-			print(f'{round(st.similarity(pt), 3):<5}', '|', st, '<->', pt)
+			if df_name == 'train':
+				print(f'{round(st.similarity(pt), 3):<5} | {row["relevance"]:<4} | {st} <-> {pt}')
+			else:
+				print(f'{round(st.similarity(pt), 3):<5} | {st} <-> {pt}')
+
 
 if __name__ == "__main__":
 	main()
